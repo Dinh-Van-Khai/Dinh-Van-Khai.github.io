0dff54f2b60b3bd39af05014d746dd49dbc2cdc2
Dinh-Van-Khai <khaingo1412@gmail.com>
2023-02-05 14:43:51 +0700

    chuyển news html,css=>json

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,20 +1,41 @@
 const newsContainer=document.querySelector('.new-container')
-const newsList=document.querySelector('.')
+const newsList=document.querySelector('.aside')
 fetch("../data/news.json")
     .then(function(response){
         return response.json()
     })
     .then(function(json){
-        var htmls=json.reduce((html,news)=>html+`
-        <div class="grid__col-9-3 mobile__col-6">
-        <a href="${news.href}" id="news${news.id}" class="news">
-            <div class="news__img"></div>
-            <h4 class="news__head">${news.head}</h4>
-        </a>
-        </div>
-        `,'')
-        newsContainer.innerHTML+=htmls
-        console.log(htmls)
+        var newsListhtml=``
+        var newsContainerhtml=``
+        json.forEach(news => {
+            var gridSize=''
+            switch(news.size){
+                case 1:
+                    gridSize='grid__col-9-3 mobile__col-6'
+                    break
+                case 2:
+                    gridSize='grid__col-9-6 mobile__col-full'
+                    break
+                case 3:
+                    gridSize='grid__col-full'
+                    break
+                default:
+                    gridSize='grid__col-9-3 mobile__col-6'
+            }
+            newsListhtml+=`
+                <li class="aside__item2"><a href="#news2">${news.head}</a></li>`
+            newsContainerhtml+=`
+                <div class="${gridSize}">
+                <a href="${news.href}" id="news${news.id}" class="news" style="background: linear-gradient(0, ${news.color}, transparent 40%),url(${news.img}) center / cover no-repeat;">
+                    <div class="news__img"></div>
+                    <h4 class="news__head">${news.head}</h4>
+                </a>
+                </div>`
+        });
+        newsContainer.innerHTML+=newsContainerhtml
+        newsList.innerHTML+=newsListhtml
+        console.log(newsContainerhtml)
+        console.log(newsListhtml)
     })
     .catch(function(fail){
         alert("Lỗi: ",fail)
